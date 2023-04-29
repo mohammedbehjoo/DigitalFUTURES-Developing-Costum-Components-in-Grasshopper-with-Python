faf0d2180ec309b7f61f6f7f73d3adf2dbfe462b
Mohammed Behjoo <48823293+mohammedbehjoo@users.noreply.github.com>
2023-04-29 09:22:53 +0330

    Update 11-colored_circles.py

--- python_scripts/11-colored_circles.py
+++ python_scripts/11-colored_circles.py
@@ -4,8 +4,13 @@ import System.Drawing as sd
 import Rhino.RhinoDoc as rr
 import scriptcontext as sc
 
+
+#get the active doc
 sc.doc=rr.ActiveDoc
 
+# x, y , and z are the coordinates of the points
+# r, g ,b specify the color of the points
+#function to create a set of colored points
 def createColoredPoint(x,y,z,r,g,b):
     currentColor = [r,g,b]
     pt = rs.AddPoint(x,y,z)
@@ -14,7 +19,7 @@ def createColoredPoint(x,y,z,r,g,b):
 
 rs.EnableRedraw(False)
 
-
+#step should be initialized as an int . it defines the distance between points
 for x in range(0,256, step):
     for y in range(0,256, step):
         for z in range(0,256,step):
